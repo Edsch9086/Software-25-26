3693fa12e9b1282a1c1ada4852764bc00aceea51
Edsch9086-XV-755 <edschaulat@gmail.com>
2025-08-05 08:11:12 -0700

    EF
    www

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World!!!");
-string moron = "wheat";
-Console.WriteLine("hello" + moron);
-Console.WriteLine($"hello" + moron);
+Console.WriteLine("what first name");
+string first = Console.ReadLine();
+Console.WriteLine("what last name");
+string last= Console.ReadLine();
+Console.WriteLine("what grade");
+string grade = Console.ReadLine();
+Console.WriteLine("what identification number");
+string id = Console.ReadLine();
+Console.WriteLine($"Last: {last}");
+Console.WriteLine($"First: {first}");
+Console.WriteLine($"Grade: {grade}");
+Console.WriteLine($"I.D: {id}");
