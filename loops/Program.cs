1eabd6ecd43bdb78436e28c81b2b21c90dd24d66
EDSCH9086 <edschaulat@gmail.com>
2025-09-04 00:36:13 +1000

    finish loops
    add tris, rhombus, box

--- loops/Program.cs
+++ loops/Program.cs
@@ -9,3 +9,70 @@ for (int i = 0; i < size; i++)
     }
     Console.WriteLine();
 }
+
+Console.WriteLine("right tri size");
+int rtri = int.Parse(Console.ReadLine());
+for (int i = 1; i <= rtri; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("inverted right tri size");
+int irtri = int.Parse(Console.ReadLine());
+for (int i = irtri; i >= 1; i--)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("rhombus size");
+int rhom = int.Parse(Console.ReadLine());
+for (int i = 1; i <= rhom; i++)
+{
+    for (int j = 0; j < rhom - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < 2 * i - 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+for (int i = rhom - 1; i >= 1; i--)
+{
+    for (int j = 0; j < rhom - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = 0; j < 2 * i - 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("box size");
+int box = int.Parse(Console.ReadLine());
+for (int i = 0; i < box; i++)
+{
+    for (int j = 0; j < box; j++)
+    {
+        if (i == 0 || i == box - 1 || j == 0 || j == box - 1)
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
