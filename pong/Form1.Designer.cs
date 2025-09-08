eccb45b25579b47023d847756167481f31666a4f
Edsch9086-XV-755 <edschaulat@gmail.com>
2025-09-08 08:26:56 -0700

    create pong
    bwaa

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.White;
+            this.p1.Location = new System.Drawing.Point(12, 12);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(16, 127);
+            this.p1.TabIndex = 0;
+            this.p1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.White;
+            this.p2.Location = new System.Drawing.Point(1236, 12);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(16, 127);
+            this.p2.TabIndex = 1;
+            this.p2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.SeaShell;
+            this.ball.Location = new System.Drawing.Point(437, 174);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 25);
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(34, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Score: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(1071, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Score: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1264, 864);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ pong/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong
+{
+    public partial class Form1 : Form
+    {
+        int p1dir = 0;
+        int p2dir = 0;
+        int balldirx = 4;
+        int balldiry = 4;
+        int score1 = 0;
+        int score2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (e.KeyChar == 'w')
+            //{
+            //    p1.Top -= 20;
+            //}
+            //else if (e.KeyChar == 's')
+            //{
+            //    p1.Top += 20;
+            //}
+            //if (e.KeyChar == 'o')
+            //{
+            //    p2.Top -= 20;
+            //}
+            //else if (e.KeyChar == 'l')
+            //{
+            //    p2.Top += 20;
+            //}
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                p1dir = -5;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                p1dir = 5;
+            }
+            if (e.KeyCode == Keys.O)
+            {
+                p2dir = -5;
+            }
+            else if (e.KeyCode == Keys.L)
+            {
+                p2dir = 5;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.S)
+            {
+                p1dir = 0;
+            }
+            if (e.KeyCode == Keys.O || e.KeyCode == Keys.L)
+            {
+                p2dir = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (p1.Top + p1dir >= 0 && p1.Top + p1dir + p1.Height <= this.ClientSize.Height)
+            {
+                p1.Top += p1dir;
+            }
+            if (p2.Top + p2dir >= 0 && p2.Top + p2dir + p2.Height <= this.ClientSize.Height)
+            {
+                p2.Top += p2dir;
+            }
+            ball.Left += balldirx;
+            ball.Top += balldiry;
+            if (ball.Top <= 0 || ball.Top + ball.Height >= this.ClientSize.Height)
+            {
+                balldiry = -balldiry;
+            }
+            if (ball.Bounds.IntersectsWith(p1.Bounds) || ball.Bounds.IntersectsWith(p2.Bounds))
+            {
+                balldirx = -balldirx;
+            }
+            if (ball.Left <= 0)
+            {
+                score2++;
+                label2.Text = $"Score: {score2}";
+                ResetBall();
+            }
+            else if (ball.Left + ball.Width >= this.ClientSize.Width)
+            {
+                score1++;
+                label1.Text = $"Score: {score1}";
+                ResetBall();
+            }
+        }
+
+        private void ResetBall()
+        {
+            ball.Left = this.ClientSize.Width / 2 - ball.Width / 2;
+            ball.Top = this.ClientSize.Height / 2 - ball.Height / 2;
+            balldirx = -balldirx;
+        }
+    }
+}
